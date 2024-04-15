44109510cb241ba0df9d015c405f1792c7cf9d27
Alex Madrazo <alex.madrazo@bgamestudios.com>
2024-04-15 11:51:39 +0200

    adding route

--- todo-list-api/src/Controller/TareaController.php
+++ todo-list-api/src/Controller/TareaController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Tarea; // Asegúrate de importar tu entidad Tarea
+use Doctrine\Persistence\ManagerRegistry;
 
 class TareaController extends AbstractController
 {
@@ -16,4 +18,45 @@ class TareaController extends AbstractController
             'path' => 'src/Controller/TareaController.php',
         ]);
     }
+
+    #[Route('/tareas', name: 'get_tareas', methods: ['GET'])]
+    public function getTareas(ManagerRegistry $doctrine): JsonResponse
+    {
+        $entityManager = $doctrine->getManager();
+        $tareas = $entityManager->getRepository(Tarea::class)->findAll();
+        $tareasArray = [];
+
+        foreach ($tareas as $tarea) {
+            $tareasArray[] = [
+                'nombre' => $tarea->getNombre(),
+                'descripcion' => $tarea->getDescripcion(),
+                'estado' => $tarea->getEstado(),
+                'identificador' => $tarea->getId(),
+            ];
+        }
+
+        return $this->json(['tareas' => $tareasArray]);
+    }
+
+    #[Route('/tarea/{id}', name: 'get_tarea', methods: ['GET'])]
+    public function getTarea(ManagerRegistry $doctrine, int $id): JsonResponse
+    {
+        $entityManager = $doctrine->getManager();
+        $tarea = $entityManager->getRepository(Tarea::class)->find($id);
+
+        if (!$tarea) {
+            return $this->json(['error' => 'Tarea not found'], 404);
+        }
+
+        $tareaArray = [
+            'nombre' => $tarea->getNombre(),
+            'descripcion' => $tarea->getDescripcion(),
+            'estado' => $tarea->getEstado(),
+            'identificador' => $tarea->getId(),
+        ];
+
+        return $this->json(['tarea' => $tareaArray]);
+    }
+
+
 }
